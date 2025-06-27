add469fa8a3e7bb15fe17dc6f43923290d56661f
Nikhilesh Satish <nik@Nikhileshs-Macbook-Air.local>
2025-06-27 23:57:14 +0530

    Updated Front-end 27/06/25

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import About from "../components/About";
+
+function App() {
+  return (
+    <>
+      <About />
+    </>
+  );
+}
+
+export default App;
